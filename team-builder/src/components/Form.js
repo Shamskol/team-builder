ff4abecaa1716540c8c73d98b89224e94af119d4
Kolade-Junaid <koladejun@yahoo.com>
2020-02-27 21:46:20 +0000

    Modified the Form.js and App.jsfiles after the initial project setup

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+export default function TeamForm(props) {
+  console.log(props);
+
+  const [teamMember, setTeamMember] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    role: "",
+  });
+
+  console.log(teamMember);
+  function submitHandler(e) {
+    e.preventDefault();
+    e.persist();
+    setTeamMember([...props.team, teamMember]);
+  }
+
+  function changeHandler(e) {
+    console.log(e.target.name);
+    setTeamMember({ ...teamMember, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <div className="TeamForm">
+      <form type="submit" onSubmit={submitHandler}>
+      <label htmlFor="form-id">id</label>
+        <input
+          type="text"
+          name="id"
+          placeholder="id"
+          value={teamMember.id}
+          onChange={changeHandler}
+        />
+        <br />
+        <label htmlFor="form-firstname">First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Enter your first name here"
+          value={teamMember.firstName}
+          onChange={changeHandler}
+        />
+        <br />
+        <label>Last Name</label>
+        <input
+          type="text"
+          name="lastname"
+          placeholder="Enter your first name here"
+          value={teamMember.lastname}
+          onChange={changeHandler}
+        />
+        <br />
+        
+
+        <label htmlFor="form-role">Role</label>
+        <input
+          type="text"
+          name="role"
+          placeholder="Enter your role here"
+          value={teamMember.role}
+          onChange={changeHandler}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
